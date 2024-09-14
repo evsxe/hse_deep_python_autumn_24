6211abea2c98772a8e2f6dbd9bdf9fca78fb5893
Evgeniy Saluev <easaluev@edu.hse.ru>
2024-09-15 00:33:43 +0300

    hw-1 fix lint

--- 01/predict_message_mood.py
+++ 01/predict_message_mood.py
@@ -6,13 +6,11 @@ class SomeModel:
         self.message = ''
         self.random_predict = uniform(0.0, 1.0)
 
-
     def get_message(self) -> str:
         """
         :return: message used for prediction
         """
         return self.message
-
 
     def predict(self, message: str) -> float:
         """
