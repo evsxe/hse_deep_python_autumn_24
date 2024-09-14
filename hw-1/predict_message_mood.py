e3b6d9a409eb705c231ce40e11e55146d18c6cfb
Evgeniy Saluev <easaluev@edu.hse.ru>
2024-09-14 19:24:47 +0300

    hw-1 update 1 task

--- hw-1/predict_message_mood.py
+++ hw-1/predict_message_mood.py
@@ -1,18 +1,43 @@
 from random import uniform
 
+"""
+Task Description:
+    Реализовать функцию predict_message_mood, которая принимает
+    на вход строку message и пороги хорошести. Функция возвращает:
+    
+    - "неуд", если предсказание модели меньше bad_threshold;
+    - "отл", если предсказание модели больше good_threshold;
+    - "норм" в остальных случаях.
+    
+    Функция predict_message_mood создает экземпляр класса SomeModel 
+    и вызывает у этого экземпляра метод predict с аргументом message.
+"""
+
 
 class SomeModel:
-    def predict(self, message: str) -> float:
-        return uniform(0.0, 1.0)
+    def __init__(self):
+        self.random_predict = uniform(0.0, 1.0)
 
+    def predict(self, message: str) -> float:
+        """
+        :param message: any string
+        :return: model prediction (random number provided by the uniform module)
+        """
+        return self.random_predict
 
 
 def predict_message_mood(
-    message: str,
-    bad_thresholds: float = 0.3,
-    good_thresholds: float = 0.8,
+        message: str,
+        bad_thresholds: float = 0.3,
+        good_thresholds: float = 0.8,
 ) -> str:
-    
+    """
+    :param message: any string
+    :param bad_thresholds: threshold for a score of "неуд"
+    :param good_thresholds: threshold for a score of "отл"
+    :return: string estimate
+    """
+
     predict = SomeModel().predict(
         message=message
     )
