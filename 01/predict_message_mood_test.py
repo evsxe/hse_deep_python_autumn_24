e8799abc7971ae1c518b38c832291b175e03438e
Evgeniy Saluev <easaluev@edu.hse.ru>
2024-09-15 10:49:24 +0300

    hw-1 pylint fix

--- 01/predict_message_mood_test.py
+++ 01/predict_message_mood_test.py
@@ -15,24 +15,24 @@ class TestPredictMessageMood(unittest.TestCase):
             predict_message_mood(123)
 
     @patch('predict_message_mood.SomeModel')
-    def test_predict_message_mood_bad(self, MockSomeModel):
-        MockSomeModel.return_value.predict.return_value = 0.2
+    def test_predict_message_mood_bad(self, mock_some_model):
+        mock_some_model.return_value.predict.return_value = 0.2
         self.assertEqual(
             predict_message_mood("Плохое настроение"),
             "неуд"
         )
 
     @patch('predict_message_mood.SomeModel')
-    def test_predict_message_mood_good(self, MockSomeModel):
-        MockSomeModel.return_value.predict.return_value = 0.9
+    def test_predict_message_mood_good(self, mock_some_model):
+        mock_some_model.return_value.predict.return_value = 0.9
         self.assertEqual(
             predict_message_mood("Отличное настроение"),
             "отл"
         )
 
     @patch('predict_message_mood.SomeModel')
-    def test_predict_message_mood_normal(self, MockSomeModel):
-        MockSomeModel.return_value.predict.return_value = 0.5
+    def test_predict_message_mood_normal(self, mock_some_model):
+        mock_some_model.return_value.predict.return_value = 0.5
         self.assertEqual(
             predict_message_mood("Нормальное настроение"),
             "норм"
