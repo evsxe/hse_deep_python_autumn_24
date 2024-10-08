566f8752922a21e9b1272751b4bae99ce49d8ac6
Evgeniy Saluev <easaluev@edu.hse.ru>
2024-10-08 19:00:31 +0300

    hw-4 fix lint

--- 04/custom_metaclass_test.py
+++ 04/custom_metaclass_test.py
@@ -1,4 +1,5 @@
 import unittest
+
 from custom_metaclass import CustomMeta
 
 
@@ -35,13 +36,22 @@ class TestCustomMeta(unittest.TestCase):
                 return "Custom_by_metaclass"
 
         inst = CustomClass()
-        inst.custom_val = 99  # type: ignore
-        inst.custom_line = inst.line  # type: ignore
-
-        self.assertEqual(inst.custom_x, 50)  # type: ignore
-        self.assertEqual(inst.custom_val, 99)  # type: ignore
-        self.assertEqual(inst.custom_line(), 100)  # type: ignore
-        self.assertEqual(str(inst), "Custom_by_metaclass")
+        self.assertEqual(
+            inst.custom_x, # type: ignore
+            50
+        )
+        self.assertEqual(
+            inst.custom_val, # type: ignore
+            99
+        )
+        self.assertEqual(
+            inst.custom_line(), # type: ignore
+            100
+        )
+        self.assertEqual(
+            str(inst),
+            "Custom_by_metaclass"
+        )
 
         with self.assertRaises(AttributeError):
             _ = inst.x
@@ -50,7 +60,7 @@ class TestCustomMeta(unittest.TestCase):
         with self.assertRaises(AttributeError):
             _ = inst.line()
         with self.assertRaises(AttributeError):
-            _ = inst.yyy  # type: ignore
+            _ = inst.yyy # type: ignore
 
     def test_dynamic_attribute_prefix(self):
         class CustomClass(metaclass=CustomMeta):
@@ -58,7 +68,6 @@ class TestCustomMeta(unittest.TestCase):
 
             def __init__(self, val=99):
                 self.val = val
-                self.dynamic = None
 
             def line(self):
                 return 100
@@ -67,11 +76,11 @@ class TestCustomMeta(unittest.TestCase):
                 return "Custom_by_metaclass"
 
         inst = CustomClass()
-        inst.dynamic = "added later"  # type: ignore
-        inst.custom_dynamic = inst.dynamic  # type: ignore
-
-        self.assertEqual(inst.custom_dynamic, "added later")  # type: ignore
-
+        inst.dynamic = "added later"
+        self.assertEqual(
+            inst.custom_dynamic, # type: ignore
+            "added later"
+        )
         with self.assertRaises(AttributeError):
             _ = inst.dynamic
 
