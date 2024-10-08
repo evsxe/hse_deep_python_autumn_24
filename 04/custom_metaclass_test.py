0852f23310929079b4e79079cb637bfa78212065
Evgeniy Saluev <easaluev@edu.hse.ru>
2024-10-08 18:26:17 +0300

    hw-4 added solution task 1

---
+++ 04/custom_metaclass_test.py
@@ -0,0 +1,71 @@
+import unittest
+from custom_metaclass import CustomMeta
+
+class TestCustomMeta(unittest.TestCase):
+
+  def test_class_attribute_prefix(self):
+    class CustomClass(metaclass=CustomMeta):
+      x = 50
+
+      def __init__(self, val=99):
+        self.val = val
+
+      def line(self):
+        return 100
+
+      def __str__(self):
+        return "Custom_by_metaclass"
+
+    self.assertEqual(CustomClass.custom_x, 50)
+    with self.assertRaises(AttributeError):
+      CustomClass.x
+
+  def test_instance_attribute_prefix(self):
+    class CustomClass(metaclass=CustomMeta):
+      x = 50
+
+      def __init__(self, val=99):
+        self.val = val
+
+      def line(self):
+        return 100
+
+      def __str__(self):
+        return "Custom_by_metaclass"
+
+    inst = CustomClass()
+    self.assertEqual(inst.custom_x, 50)
+    self.assertEqual(inst.custom_val, 99)
+    self.assertEqual(inst.custom_line(), 100)
+    self.assertEqual(str(inst), "Custom_by_metaclass")
+
+    with self.assertRaises(AttributeError):
+      inst.x
+    with self.assertRaises(AttributeError):
+      inst.val
+    with self.assertRaises(AttributeError):
+      inst.line()
+    with self.assertRaises(AttributeError):
+      inst.yyy
+
+  def test_dynamic_attribute_prefix(self):
+    class CustomClass(metaclass=CustomMeta):
+      x = 50
+
+      def __init__(self, val=99):
+        self.val = val
+
+      def line(self):
+        return 100
+
+      def __str__(self):
+        return "Custom_by_metaclass"
+
+    inst = CustomClass()
+    inst.dynamic = "added later"
+    self.assertEqual(inst.custom_dynamic, "added later")
+    with self.assertRaises(AttributeError):
+      inst.dynamic
+
+if __name__ == '__main__':
+  unittest.main()
