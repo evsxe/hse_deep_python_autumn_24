7818c954d065446a4b5c38228e3e79071d7b7ac8
e.saluev <e.saluev@ntc-vulkan.ru>
2024-11-01 13:45:18 +0300

    HW-6 init solution hw

---
+++ 06/client.py
@@ -0,0 +1,63 @@
+import sys
+import socket
+import threading
+
+from urllib.parse import urlparse
+
+
+# Start: python client.py 10 urls.txt
+
+
+class RequestThread(threading.Thread):
+    def __init__(self, url, result):
+        super().__init__()
+        self.url = url
+        self.result = result
+
+    def run(self):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect(('127.0.0.1', 8080))
+                sock.sendall(self.url.encode())
+                data = sock.recv(4096)
+                self.result[self.url] = data.decode()
+        except Exception as e:
+            print(f"Error connecting to {self.url}: {e}")
+
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python client.py select count urls.txt")
+        return
+
+    num_threads = int(sys.argv[1])
+    url_file = sys.argv[2]
+
+    with open(url_file) as f:
+        urls = [line.strip() for line in f.readlines()]
+
+    threads = []
+    results = {}
+
+    for url in urls:
+        if urlparse(url).scheme in ['http', 'https']:
+            if len(threads) >= num_threads:
+                for thread in threads:
+                    thread.join()
+                threads = []
+
+            thread = RequestThread(url, results)
+            thread.start()
+            threads.append(thread)
+        else:
+            print(f"Некорректный URL: {url}")
+
+    for thread in threads:
+        thread.join()
+
+    for url, response in results.items():
+        print(f"{url}: {response}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ 06/server.py
@@ -0,0 +1,87 @@
+import json
+import queue
+import socket
+import requests
+import threading
+
+from collections import Counter
+from urllib.parse import urlparse
+
+
+# Start: python server.py -w 10 -k 7
+
+
+class Worker(threading.Thread):
+    def __init__(self, task_queue, result_queue):
+        super().__init__()
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    def run(self):
+        while True:
+            url = self.task_queue.get()
+            if url is None:
+                break
+            print(f"URL Handling: {url}")
+            top_words = self.process_url(url)
+            self.result_queue.put((url, top_words))
+            self.task_queue.task_done()
+
+    @staticmethod
+    def process_url(url):
+        try:
+            response = requests.get(url)
+            text = response.text
+            words = Counter(text.split())
+            return dict(words.most_common(7))
+        except Exception as e:
+            print(f"Processing error {url}: {e}")
+            return {}
+
+
+class Master:
+    def __init__(self, port, num_workers, k):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('0.0.0.0', port))
+        self.server_socket.listen(5)
+        self.task_queue = queue.Queue()
+        self.result_queue = queue.Queue()
+        self.active_requests = 0
+        self.k = k
+
+        self.workers = [Worker(self.task_queue, self.result_queue) for _ in range(num_workers)]
+        for worker in self.workers:
+            worker.start()
+
+    def start(self):
+        print("Server is running...")
+        while True:
+            conn, addr = self.server_socket.accept()
+            print(f"Connected {addr}")
+            try:
+                url = conn.recv(1024).decode()
+                if url:
+                    if urlparse(url).scheme in ['http', 'https']:
+                        self.active_requests += 1
+                        self.task_queue.put(url)
+                        top_words = self.result_queue.get()
+                        response = json.dumps(top_words[1])
+                        conn.sendall(response.encode())
+                        print(f"URL processed: {self.active_requests}")
+                    else:
+                        conn.sendall("Incorrect URL".encode('utf-8'))
+                        print(f"Incorrect URL: {url}")
+            finally:
+                conn.close()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', type=int, help="Number of Workers", required=True)
+    parser.add_argument('-k', type=int, help="Top K Frequent Words", required=True)
+    args = parser.parse_args()
+
+    server = Master(8080, args.w, args.k)
+    server.start()
