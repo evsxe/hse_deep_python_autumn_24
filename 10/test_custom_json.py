5c66f81ba867fb24a3cf1bccc35141d7e259d59f
e.saluev <e.saluev@ntc-vulkan.ru>
2024-12-18 23:54:23 +0300

    HW-10 fix test

--- 10/test_custom_json.py
+++ 10/test_custom_json.py
@@ -15,14 +15,9 @@ class TestCustomJson(unittest.TestCase):
         ]
 
         for case in valid_cases:
-            try:
-                expected = json.loads(case)
-                actual = custom_json.loads(case)
-                self.assertEqual(expected, actual, f"Failed for input: {case}")
-            except ValueError as e:
-                self.fail(
-                    f"Unexpected ValueError for valid JSON: {case}, {e}"
-                )
+            expected = json.loads(case)
+            actual = custom_json.loads(case)
+            self.assertEqual(expected, actual, f"Failed for input: {case}")
 
     def test_loads_dumps_roundtrip(self):
         data_cases = [
